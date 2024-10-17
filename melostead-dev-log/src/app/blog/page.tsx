05a719b6772d710f3475716f11ab808f5f14a334
hanoelleb <36749694+hanoelleb@users.noreply.github.com>
2024-10-16 22:09:57 -0700

    initial commit

---
+++ melostead-dev-log/src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../page";
+
+export default function Blog() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <h1>Melostead Blog</h1>
+    </>
+  );
+}
---
+++ melostead-dev-log/src/app/media/page.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../page";
+
+export default function Media() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <h1>Media</h1>
+    </>
+  );
+}
